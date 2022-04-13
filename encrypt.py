98aa2f4f3c634e49d59059dbd1f403f9c20b22d4
karthik nair <90816230+karthik4579@users.noreply.github.com>
2022-04-13 18:56:29 +0530

    Refactor encrypt.py
    * Removed uncessary logic 
    *Return a string instead of a byte string (fix)

--- encrypt.py
+++ encrypt.py
@@ -2,34 +2,7 @@ from cryptography.fernet import Fernet
 key = Fernet.generate_key()
 fernet = Fernet(key)
 
-
-def ncrypt(*args):
-    temp = []
-    for n in args:
-        temp.append(n)
-    size = len(temp)
-
-    newtemp = []
-    if size > 1:
-
-            temp1 = fernet.encrypt(temp[0].encode())
-            newtemp.append(temp1)
-
-            temp2 = fernet.encrypt(temp[1].encode())
-            newtemp.append(temp2)
-
-            """
-            Just there for just in case there is a problem returning a whole list
-            a = newtemp[0]
-            b = newtemp[1]
-            """
-            return tuple(newtemp)
-
-    else:
-        temp3 = fernet.encrypt(temp[0].encode())
-        return temp3
-
-
-def dcrypt(encMessage):
-    decMessage = fernet.decrypt(encMessage).decode()
-    return decMessage
+def ncrypt(a):
+    temp1 = fernet.encrypt(a.encode())
+    b = temp1.decode()
+    return b
